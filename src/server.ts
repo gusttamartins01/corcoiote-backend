5c92fac79d0ca29036493f9ae3f78b6bbf9d7573
Gustavo Martins <luizgustams@gmail.com>
2026-06-25 16:27:40 -0300

    refactory: atualizar servidor com estrutura de abstracao

--- src/server.ts
+++ src/server.ts
@@ -1,3 +1,16 @@
-import http from 'node:http';
+import express, { response } from 'express';
+import { users } from './mocks/users.ts';
 
-http.createServer(() => console.log('Knocked on the door!')).listen(3333);
+const app = express();
+
+app.use(express.json());
+
+app.get('/users', (_request, response) => {
+	response.status(200).json(users);
+});
+
+app.use((_request, response) => {
+	response.status(404).json({ messege: 'Not found!' });
+});
+
+app.listen(Number(process.env.PORT));
