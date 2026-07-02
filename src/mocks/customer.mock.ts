7b02863ad30be62fbc8a739a2e5a2c1b5efdb6b7
Gustavo Martins <luizgustams@gmail.com>
2026-07-02 16:41:19 -0300

    refactor: mover dados simulados para diretorio dedicado

---
+++ src/mocks/customer.mock.ts
@@ -0,0 +1,22 @@
+import type { Customer } from '../types.ts';
+
+export const customers: Customer[] = [
+	{
+		id: 1,
+		name: 'Cristiano Ronaldo',
+		email: 'robozao@email.com',
+		status: true,
+	},
+	{
+		id: 2,
+		name: 'Lionel Messi',
+		email: 'leo@email.com',
+		status: true,
+	},
+	{
+		id: 3,
+		name: 'Neymar Júnior',
+		email: 'ney@email.com',
+		status: false,
+	},
+];
