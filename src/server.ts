e88ea5eb6e72b49f225d6d31414e59f785a3ef7b
gustavo martins <luizgustams@gmail.com>
2026-07-09 23:57:25 -0300

    feat: criar caminho de faturas no servidor

--- src/server.ts
+++ src/server.ts
@@ -1,5 +1,6 @@
 import express from 'express';
 import CustomerRouter from './routes/customer.router.ts';
+import InvoiceRouter from './routes/invoice.router.ts';
 
 const app = express();
 
@@ -7,10 +8,16 @@ app.use(express.json());
 
 app.use('/customers', CustomerRouter);
 
+app.use('/invoices', InvoiceRouter);
+
 app.use((_request, response) => {
 	response.status(404).json({
 		messege: 'Not found!',
 	});
 });
 
-app.listen(Number(process.env.PORT));
+const PORT = Number(process.env.PORT) || 3333;
+
+app.listen(PORT, () => {
+	console.log(`Server running on port: http://localhost:${PORT}`);
+});
