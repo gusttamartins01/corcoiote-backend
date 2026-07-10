18f4ebba9b3428239cafe7d36721f61341f2559a
gustavo martins <luizgustams@gmail.com>
2026-07-09 22:25:55 -0300

    feat: criar modelo reutilizavel de invoices

--- src/types.ts
+++ src/types.ts
@@ -7,3 +7,11 @@ export type Customer = {
 
 export type CreateCustomer = Pick<Customer, 'name' | 'email'>;
 export type UpdateCustomer = Partial<Omit<Customer, 'id'>>;
+
+export type Invoices = {
+	id: number;
+	value: string;
+	customer_id: number;
+	status: 'Paid' | 'Pending';
+	created_at: string;
+};
