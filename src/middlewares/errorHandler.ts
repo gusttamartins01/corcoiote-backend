c13fb152f3cb73c332f4df4af6e7a3e78c79d058
Gustavo Martins <luizgustams@gmail.com>
2026-07-30 16:26:10 -0300

    refector: alterar registrador de erro desconhecido

--- src/middlewares/errorHandler.ts
+++ src/middlewares/errorHandler.ts
@@ -1,5 +1,6 @@
 import type { NextFunction, Request, Response } from 'express';
 import { NotFoundError, ValidationError } from '../errors/index.ts';
+import logger from '../lib/logger.ts';
 
 export default function errorHandler(
 	error: unknown,
@@ -19,7 +20,7 @@ export default function errorHandler(
 		});
 	}
 
-	console.log(error);
+	logger.error(error);
 
 	response.status(500).json({ message: 'Erro interno do servidor.' });
 }
