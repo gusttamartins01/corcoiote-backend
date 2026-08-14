57421814305f38770774b3b658ebc648a58396e3
Gustavo Martins <luizgustams@gmail.com>
2026-08-14 18:57:50 -0300

    refactor: criar novos dados mockados  de clientes

--- src/mocks/customer.mock.ts
+++ src/mocks/customer.mock.ts
@@ -3,20 +3,26 @@ import type { Customer } from '../types.ts';
 export const customers: Customer[] = [
 	{
 		id: 1,
+		name: 'Gustavo Martins',
+		email: 'gustavo@email.com',
+		status: true
+	},
+	{
+		id: 2,
 		name: 'Cristiano Ronaldo',
 		email: 'robozao@email.com',
 		status: true
 	},
 	{
-		id: 2,
-		name: 'Lionel Messi',
-		email: 'leo@email.com',
+		id: 3,
+		name: 'Stephen Curry',
+		email: 'curry@email.com',
 		status: true
 	},
 	{
-		id: 3,
-		name: 'Neymar Júnior',
-		email: 'ney@email.com',
+		id: 4,
+		name: 'Patrick Moahomes',
+		email: 'mahomes@email.com',
 		status: false
 	}
 ];
