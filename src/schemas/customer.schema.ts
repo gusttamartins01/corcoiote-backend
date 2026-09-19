49052bfd9c6ba462b0289a578a5d88a32007d5f1
Gusttavo Martins <luizgustams@gmail.com>
2026-09-19 00:21:45 +0000

    refactor: alterar a forma de criação de updateschema.

--- src/schemas/customer.schema.ts
+++ src/schemas/customer.schema.ts
@@ -8,14 +8,7 @@ export const createCustomerSchema = z.object({
 	imageUrl: z.url('URL inválida.').optional()
 });
 
-export const updateCustomerSchema = z.object({
-	name: z
-		.string('Entrada inválida: Esperava-se um texto')
-		.min(1, 'Muito curto: esperava-se um texto com ao menos 1 caractere')
-		.optional(),
-	email: z.email('Endereço de e-mail inválido').optional(),
-	imageUrl: z.url('URL inválida.').optional()
-});
+export const updateCustomerSchema = createCustomerSchema.partial();
 
 export type CreateCustomer = z.infer<typeof createCustomerSchema>;
 export type UpdateCustomer = z.infer<typeof updateCustomerSchema>;
