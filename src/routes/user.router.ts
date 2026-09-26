e4deed7e9fde31dc4d9ab3bc77e723dea51d8bb2
Gusttavo Martins <luizgustams@gmail.com>
2026-09-26 01:56:36 +0000

    feat: criar logica da camada de routers  de usuarios.

---
+++ src/routes/user.router.ts
@@ -0,0 +1,14 @@
+import { Router } from 'express';
+import * as UserController from '../controllers/user.controller.ts';
+import validate from '../middlewares/validate.ts';
+import { createUserSchema, updateUserSchema } from '../schemas/user.schema.ts';
+
+const router = Router();
+
+router.get('/', UserController.getAllUsers);
+router.get('/:id', UserController.getUserById);
+router.post('/', validate(createUserSchema), UserController.createUser);
+router.put('/:id', validate(updateUserSchema), UserController.updateUser);
+router.delete('/:id', UserController.deleteUSer);
+
+export default router;
